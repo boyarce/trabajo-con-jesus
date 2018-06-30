50e1e9798f3ccdc2ebb52516e29689a8f6aa907d
boyarce <b.nelson@alumnos.duoc.cl>
2018-06-30 01:43:10 -0400

    holen 5

---
+++ Resources/resources/model/Carne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resources.resources.model
+{
+    public class Carne : Producto
+    {
+        private char categoria;
+        private double descuentoCategoria;
+
+        public Carne(char categoria, double descuentoCategoria, int cantidad, string nombre, int precio) : base(cantidad, nombre, precio)
+        {
+            Categoria = categoria;
+            DescuentoCategoria = descuentoCategoria;
+        }
+
+        public char Categoria { get => categoria; set => categoria = value; }
+        public double DescuentoCategoria { get => descuentoCategoria; set => descuentoCategoria = value; }
+    }
+}
---
+++ Resources/resources/model/FrutaVerdura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resources.resources.model
+{
+    public class FrutaVerdura : Producto
+    {
+        private string tipo;
+
+        public FrutaVerdura(string tipo, int cantidad, string nombre, int precio) : base(cantidad, nombre, precio)
+        {
+            Tipo = tipo;
+        }
+
+        public string Tipo { get => tipo; set => tipo = value; }
+    }
+}
