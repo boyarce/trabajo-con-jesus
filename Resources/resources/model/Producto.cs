edb742e718654348aee359f78eea0e3156042eed
boyarce <b.nelson@alumnos.duoc.cl>
2018-06-30 00:55:43 -0400

    holen 2

--- Resources/resources/model/Producto.cs
+++ Resources/resources/model/Producto.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Resources.resources.model
 {
-    class Producto
+    public class Producto
     {
+        private int cantidad;
+        private string nombre;
+        private int precioUnitario;
+
+        public Producto(int cantidad, string nombre, int precio)
+        {
+            Cantidad = cantidad;
+            Nombre = nombre;
+            PrecioUnitario = precioUnitario;
+        }
+
+        public int Cantidad { get => cantidad; set => cantidad = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int PrecioUnitario { get => precioUnitario; set => precioUnitario = value; }
+
+        public override string ToString()
+        {
+            return "Cantidad: "+cantidad+" "+"Nombre: "+nombre+" "+"Precio-Unitario: "+precioUnitario;
+        }
     }
 }
